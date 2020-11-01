fdced7aab13706bb0bbe3cbf7f65e43f6418e719
Syed Asad Amin <s.asad.amin@gmail.com>
2020-11-01 15:55:58 +0500

    Initial Commit.

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "main.h"
+
+using namespace std;
+
+int main(void) {
+    fprintf(stdout, "Application started...\r\n");
+    
+    fprintf(stdout, "Application finished...\r\n");
+    return EXIT_SUCCESS;
+}
