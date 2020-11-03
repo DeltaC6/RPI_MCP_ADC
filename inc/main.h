05f27683a0791445b2993a45988d9d141838bded
Syed Asad Amin <s.asad.amin@gmail.com>
2020-11-04 03:16:01 +0500

    Ring buffer added. Changed writer opeations.

--- inc/main.h
+++ inc/main.h
@@ -7,21 +7,31 @@
  *          | v1.0.1 -> Adding methods to read ADC data form MCP3008 using SPI
  *          | v1.0.2 -> Created reader and writer threads for synced operations
  *          | v1.0.3 -> Added reader and writer routines
- *          |
+ *          | v1.0.4 -> Completely removed the file access time by making it
+ *                      global. Added a ring buffer operation instead of a
+ *                      simple buffer accessed by locked states
+ *          | v1.0.5 -> Text file is changed to binary file since creating and
+ *                      writing text is too time consuming
  * 
  * @note    This is a program written in C++ to read ADC data from IC MCP3008
  *          via SPI. MCP3008 is 8 channel, 10bit with 200KSPS max sampling rate
  * 
- *          This application read data from 4 chanels at greater than 40KSPS
+ *          This application read data from 4 channels at greater than 40KSPS
  *          sampling rate and write it to a log file
  * 
- *          Time management for reader and writer threads is completed
- *          To close the threads safely just reset the isReaderRunning and 
+ *          Time management for reader and writer threads is completed. To
+ *          close the threads safely just reset the isReaderRunning and 
  *          isWriterRunning flags
  * 
- *  A project for fiverr client
+ *          TIME RECORDED UPON TEST (STILL LOSSY BUT ACCESPTABLE)
+ *          Binary file operation wrote 16384 samples in 2 seconds
  * 
+ *          [PERSONAL NOTE] This could get better if I patch the RPI kernel
+ *          with RTPremept kernel module
+ * 
+ *  A project for fiverr client
  */
+
 
 #pragma once
 
@@ -52,12 +62,4 @@ void adcReader(void);
  * @return None
  */
 void fileWriter(void);
-
-/*!
- * @brief This function writes log data to a file
- * @param [const char *] Name of file to write to
- * @param [const char *] Data to write into the file
- * @return None
- */
-void writeFile(const char *file, const char *data);
 //============================================================================//
--- src/main.cpp
+++ src/main.cpp
@@ -7,37 +7,54 @@
  *          | v1.0.1 -> Adding methods to read ADC data form MCP3008 using SPI
  *          | v1.0.2 -> Created reader and writer threads for synced operations
  *          | v1.0.3 -> Added reader and writer routines
- *          |
+ *          | v1.0.4 -> Completely removed the file access time by making it
+ *                      global. Added a ring buffer operation instead of a
+ *                      simple buffer accessed by locked states
+ *          | v1.0.5 -> Text file is changed to binary file since creating and
+ *                      writing text is too time consuming
  * 
  * @note    This is a program written in C++ to read ADC data from IC MCP3008
  *          via SPI. MCP3008 is 8 channel, 10bit with 200KSPS max sampling rate
  * 
- *          This application read data from 4 chanels at greater than 40KSPS
+ *          This application read data from 4 channels at greater than 40KSPS
  *          sampling rate and write it to a log file
  * 
  *          Time management for reader and writer threads is completed. To
  *          close the threads safely just reset the isReaderRunning and 
- *          isWriterRunning flags.
+ *          isWriterRunning flags
+ * 
+ *          TIME RECORDED UPON TEST (STILL LOSSY BUT ACCESPTABLE)
+ *          Binary file operation wrote 16384 samples in approx 2 seconds
  * 
  *  A project for fiverr client
- * 
  */
 
 #include "main.h"
 
 using namespace std;
 
-bool isReaderRunning = true;
-bool isWriterRunning = true;
+bool isReaderRunning = false;
+bool isWriterRunning = false;
 
 pthread_t readerThread;
 pthread_t writerThread;
-mutex dataMutex;
 
-static const char logPath[] = "/home/pi/Desktop/MCP3008.log";
+static const char logPath[] = "/home/pi/Desktop/MCP3008.bin";
+
 static const uint8_t NO_OF_CHANNEL = 4;
-bool dataRdyFlag = false;
-int16_t adcData[NO_OF_CHANNEL];
+static const uint16_t SAMPLE_PER_CHANNEL = 10000;
+static uint16_t adcData[NO_OF_CHANNEL][SAMPLE_PER_CHANNEL];
+
+static uint8_t curChannel = 0;
+static uint16_t curSample = 0;
+
+// Ring buffer for IO stream
+bool isFullFlag = false;
+static uint16_t writePTR = 0;
+static uint16_t readPTR = 0;
+
+// Filestream
+ofstream mFile;
 
 int main(void) {
     fprintf(stdout, "Application started...\r\n");
@@ -45,15 +62,18 @@ int main(void) {
     initGPIO();
     initSPI(SPI_CHANNEL_1, SPI_CLK_SPD);
 
+    mFile.open(logPath, ios::binary | ios::out | ios::app);
+
     thread readerThread(adcReader);
     thread writerThread(fileWriter);
 
     if(readerThread.joinable()) { readerThread.join(); }
     if(writerThread.joinable()) { writerThread.join(); }
-
-    // Program should never reach here when using thread until they are closed
+    
+    delay(1000);
     deinitSPI();
     deinitGPIO();
+    mFile.close();
     
     fprintf(stdout, "Application finished...\r\n");
     return EXIT_SUCCESS;
@@ -61,26 +81,28 @@ int main(void) {
 
 void adcReader(void) {
     fprintf(stdout, "Reader thread started...\r\n");
-
-    static uint8_t curChannel = 0;
+    isReaderRunning = true;
     while(isReaderRunning) {
+        if(isFullFlag) { continue; }
+
         int16_t val = readADC(SPI_CHANNEL_1, CHAN_CONF_SING, curChannel);
+        adcData[curChannel][curSample] = val;
 
-        dataMutex.lock();
-        adcData[curChannel] = val;
-        dataMutex.unlock();
-
+        // Buffer the data into RAM before writing to file
         curChannel++;
-        if(curChannel >= NO_OF_CHANNEL) { 
+        if(curChannel >= NO_OF_CHANNEL) {
             curChannel = 0;
-            dataRdyFlag = true;
+            curSample++;
+            if(curSample >= SAMPLE_PER_CHANNEL) { curSample = 0; }
+
+            writePTR = (writePTR + 1) % SAMPLE_PER_CHANNEL;
+            if(writePTR == readPTR) { isFullFlag = true; }
         }
-        
+
         // This time management routine will compensate for sample capacitor
         // charge time as well
-        // Time = 5Tau = 5RC = 5 * 1e3 * 20e-12 = 0.12us
-        // Chnage time as low as 1us to increase sampled data
-        std::this_thread::sleep_for(std::chrono::microseconds(10));
+        // Time = 5Tau = 5RC = 5 * 1e3 * 20e-12 = 100ns
+        std::this_thread::sleep_for(std::chrono::nanoseconds(100));
     }
 
     fprintf(stdout, "Reader thread finished...\r\n");
@@ -90,36 +112,18 @@ void fileWriter(void) {
     fprintf(stdout, "Writer thread started...\r\n");
 
     char msgStr[64];
-    char tempStr[32];
     memset(msgStr, 0, sizeof(msgStr));
-
-    // Creating header for CSV file
-    sprintf(msgStr, "CH1,CH2,CH3,CH4,\r\n");
-    writeFile(logPath, msgStr);
-
+    isWriterRunning = true;
     while(isWriterRunning) {
-        if(dataRdyFlag) {
-            memset(msgStr, 0, sizeof(msgStr));
+        if((writePTR == readPTR) && !isFullFlag) { continue; }
 
-            dataMutex.lock();
-            for(int i = 0; i < NO_OF_CHANNEL; i++) {
-                sprintf(tempStr, "%d,", adcData[i]);
-                strcat(msgStr, tempStr);
-            }
-            dataMutex.unlock();
-
-            writeFile(logPath, msgStr);
-            dataRdyFlag = false;
+        msgStr[0] = 0;
+        for(int i = 0; i < NO_OF_CHANNEL; i++) {
+            //sprintf(msgStr, "%s%d,", msgStr, adcData[i][readPTR]);
+            mFile.write((char *) &adcData[i][readPTR], sizeof(uint16_t));
         }
-        std::this_thread::sleep_for(std::chrono::microseconds(1));
+        readPTR = (readPTR + 1) % SAMPLE_PER_CHANNEL;
+        isFullFlag = false;
     }
-
     fprintf(stdout, "Writer thread finished...\r\n");
-}
-
-void writeFile(const char *file, const char *data) {
-    ofstream mFile;
-    mFile.open(file, ios::out | ios::app);
-    mFile << data << endl;
-    mFile.close();
 }
